31d6b789d57175fad373ba96612f08904e977e31
maksudfataliyev <143101499+maksudfataliyev@users.noreply.github.com>
2026-01-08 19:05:18 +0400

    Delete iCasesFinal_JWT directory

--- iCasesFinal_JWT/src/components/getImageUrl.ts
+++
@@ -1,7 +0,0 @@
-export function getProductImageUrl(imagePath: string): string {
-  if (!imagePath) return '';
-
-  if (imagePath.startsWith('http')) return imagePath;
-
-  return new URL(`../assets/images/${imagePath}`, import.meta.url).href;
-}
